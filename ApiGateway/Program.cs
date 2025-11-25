2fa14d9aa7349a1897a3400a0168bb996ed56b8a
Vu The Vinh <vinhvu20102005@gmail.com>
2025-11-25 21:28:08 +0700

    Fix UseSqlServer in DbContextFactory

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// 1. Cấu hình Ocelot để đọc file định tuyến ocelot.json
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// 2. Thêm dịch vụ Ocelot
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// 3. Không dùng Swagger/Controllers/HTTPS/Authorization (chỉ định tuyến)
+// app.UseSwagger();
+// app.UseSwaggerUI();
+// app.UseHttpsRedirection();
+// app.UseAuthorization();
+// app.MapControllers();
+
+// 4. Kích hoạt Ocelot Middleware (Bắt buộc phải là await và đặt ở cuối)
+await app.UseOcelot();
+
+app.Run();
--- ShortenUrl.Tests/obj/Debug/net8.0/ShortenUrl.Tests.AssemblyInfo.cs
+++ ShortenUrl.Tests/obj/Debug/net8.0/ShortenUrl.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090bf2b1d66fd746989809ba484f6c6065e72b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c333f6bec12ecb532ffd285ea2ea9ad20097d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
