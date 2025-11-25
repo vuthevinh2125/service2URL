33745e0b1d58485fafe6f75938104ac0e0719529
Vu The Vinh <vinhvu20102005@gmail.com>
2025-11-25 20:06:00 +0700

    Fix UseSqlServer to UseNpgsql in Factory

--- ShortenUrl.Tests/obj/Debug/net8.0/ShortenUrl.Tests.AssemblyInfo.cs
+++ ShortenUrl.Tests/obj/Debug/net8.0/ShortenUrl.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc1022d67a3e820e537aedcec89939452d532b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7ee3f50d10546962e89474701ca75071af38bd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
