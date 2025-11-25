c8e50d3d6bb5755b94e39127551246c675ae0267
Vu The Vinh <vinhvu20102005@gmail.com>
2025-11-25 17:32:59 +0700

    Add dockerignore and fix Dockerfile paths

--- ShortenUrl.Tests/obj/Debug/net8.0/ShortenUrl.Tests.AssemblyInfo.cs
+++ ShortenUrl.Tests/obj/Debug/net8.0/ShortenUrl.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb9b6c12fff365ccfd54325c1a4802361bda5a29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f85708578eef555a2a2b08da73d381c9671c74bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShortenUrl.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
